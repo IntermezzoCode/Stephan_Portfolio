6facce0733e60803951bf11be9c78d7b72563725
IntermezzoCode <schmidbauer.stephan@gmail.com>
2018-03-05 16:48:25 +0100

    startup scraper

---
+++ scraper.rb
@@ -0,0 +1,31 @@
+require "open-uri"
+require "nokogiri"
+require "csv"
+
+def scraper
+
+  url = "https://berlin.startups-list.com/"
+  companies = []
+  attributes = {}
+
+  doc = Nokogiri::HTML(open(url))
+  doc.css('.card h1').each do |element|
+    p element.text.strip
+    p element.attribute('data-href')
+  end
+
+ #  doc.css('.card').each do |element|
+ #    # p element.text.strip
+ #    p element.attribute('data-href').value
+ #  end
+
+ # doc.css('.fa-facebook-square:parent').each do |element|
+ #    p element
+ #    # p element.attribute('data-href').value
+ #  end
+
+
+
+end
+
+scraper
